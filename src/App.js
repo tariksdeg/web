680fd10067dbeb35cb597d99ab91966da8c32635
tariksdeg <tariksdegirmencii@gmail.com>
2023-01-26 21:09:29 +0300

    web

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [dbData, setDbData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/")
+      .then((res) => setDbData(res.data))
+      .catch((err) => console.log(`err0 ==>`, err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Welcome React with Docker</h1>
+      {dbData.length > 0 ? (
+        dbData.map(({ name, age }, index) => (
+          <ul key={index}>
+            <li>{name}</li>
+            <li>{age}</li>
+            <hr />
+          </ul>
+        ))
+      ) : (
+        <div>There is no results</div>
+      )}
     </div>
   );
 }
